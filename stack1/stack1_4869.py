53118d749ac561332bcbc6bdfa134dcb069affb7
Lee-JunH <mayday_jh@naver.com>
2025-07-31 17:51:47 +0900

    stack1 4869

--- stack1/stack1_4869.py
+++ stack1/stack1_4869.py
@@ -2,7 +2,20 @@
 
 T = int(input())
 
+def func(N, count):
+    if N == 0:
+        return 1
+    if N < 0:
+        return 0
+    count += func(N - 10, count) + 2 * func(N - 20, count)
+    return count
+
 for case in range(T):
     N = int(input())
 
-    
+    result = []
+    i = 0
+    j = 0
+    count = func(N, 0) 
+            
+    print(f'#{case+1} {count}')
