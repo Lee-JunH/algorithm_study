70a1e8577a65ab27598887e433d133f9ab4c6eba
junh <harry.lee1006@gmail.com>
2025-08-03 23:48:31 +0900

    stack2 study

--- stack2/stack2_4874.py
+++ stack2/stack2_4874.py
@@ -5,9 +5,22 @@ SWEA 4874번 - Forth(D2)
 - 후위 표기법으로 숫자, 연산자, '.' 을 입력받아 결과를 출력
 '''
 
-def calculate(data):
-    if data == '+':
-        pass
+def calculate(my_list, data):
+    try:
+        temp1 = my_list.pop()
+        temp2 = my_list.pop()
+    except IndexError:
+        return 'error'
+    else:
+        if data == '+':
+            return temp2 + temp1
+        elif data == '-':
+            return temp2 - temp1
+        elif data == '*':
+            return temp2 * temp1
+        elif data == '/':
+            return temp2 / temp1
+        
 
 T = int(input())
 for case in range(T):
@@ -16,17 +29,17 @@ for case in range(T):
     my_list = []
     for data in lst:
         if data in '+-/*':
-            try:
-                my_list.append(my_list.pop() + my_list.pop())
-            except IndexError:
-                result = 'error'
+            result = calculate(my_list, data)
+            if result == 'error':
                 break
+            else:
+                my_list.append(result)
         elif data == '.':
             break
         else:
             my_list.append(int(data))
-    if my_list.count != 1:
-        result = 'error'
-    else:
+    if len(my_list) == 1:
         result = my_list.pop()
+    else:
+        result = 'error'
     print(f'#{case+1} {result}')
