7c0986119598d94d8c32896a0abd47d01c888e37
Lee-JunH <harry.lee1006@gmail.com>
2025-08-08 15:41:02 +0900

    GNS update

--- algo_class/String/1221_GNS.py
+++ algo_class/String/1221_GNS.py
@@ -17,8 +17,7 @@ for tc in range(T):
 
     my_dict = {"ZRO": 0, "ONE": 1, "TWO": 2, "THR": 3, "FOR": 4, \
                "FIV": 5, "SIX": 6, "SVN": 7, "EGT": 8, "NIN": 9}
-    my_dict2 = {0: "ZRO", 1: "ONE", 2: "TWO", 3: "THR", 4: "FOR", \
-                5: "FIV", 6: "SIX", 7: "SVN", 8: "EGT", 9: "NIN"}
+    my_dict2 = {value: key for key, value in my_dict.items()}
     for num in num_lst:
         number[my_dict[num]] += 1
     print(f'#{tc+1} ', end='')
